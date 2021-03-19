a3cede6a62079507749e8db44644b82f3e6d2e9e
avery <asommer999@gmail.com>
2021-03-18 20:12:34 -0400

    some styling

--- quotes.js
+++ quotes.js
@@ -1,11 +1,11 @@
 var insert_stories = (data) => {
-  var story_feed = document.getElementById("quotes_feed");
+  var story_feed = document.getElementById("quotes_feed").children;
   var i = 0;
 
   for(var t = 0; t < data.length; t++){
     var embed = data[t]["content"]["$t"];
-
-    story_feed.innerHTML += embed;
+    embed = "<p class=\"section_text\">\"" + embed + "\"</p>";
+    story_feed[t%2].innerHTML += embed;
   }
 }
 
@@ -13,4 +13,3 @@ fetch("https://spreadsheets.google.com/feeds/cells/1G1O8-CIhHx_JBrHfHpeEldaMWUTm
   .then(response => response.json())
   .then(data => data["feed"]["entry"])
   .then(data => insert_stories(data))
-  // .then(_ => window.instgrm.Embeds.process());
