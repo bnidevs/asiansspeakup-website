a548a692e4249d2ee86193aa3776a78ee2912000
Bill Ni <bill.8ni@gmail.com>
2021-08-22 17:28:07 -0400

    update linkinbio

--- linkinbio/linkinbio.js
+++ linkinbio/linkinbio.js
@@ -1,12 +1,17 @@
 var insert_linkinbio = (data) => {
   var feed = document.getElementById("linkinbio_links");
 
-  for(var t = 0; t < data.length; t += 2){
-    feed.innerHTML += '<a href="' + data[t]["content"]["$t"] + '"><button class="button linkinbio">' + data[t+1]["content"]["$t"] + '</button></a>';
+  for(var t = 0; t < data.length; t++){
+    var row = data[t];
+
+    var content = row["c"];
+    
+    feed.innerHTML += '<a href="' + content[1]['v'] + '"><button class="button linkinbio">' + content[0]['v'] + '</button></a>';
   }
 }
 
-fetch("https://spreadsheets.google.com/feeds/cells/1QLjapJmsyUcygN7JoAC_MMdCo-mveIFDrmN9tiSXRIM/1/public/values?alt=json")
-  .then(response => response.json())
-  .then(data => data["feed"]["entry"])
+fetch("https://docs.google.com/spreadsheets/d/1QLjapJmsyUcygN7JoAC_MMdCo-mveIFDrmN9tiSXRIM/gviz/tq?tqx=out:json&sheet=Sheet1")
+  .then(response => response.text())
+  .then(response => JSON.parse(strip(response)))
+  .then(data => data["table"]["rows"])
   .then(data => insert_linkinbio(data))
